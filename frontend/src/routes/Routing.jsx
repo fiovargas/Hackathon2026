6359e07111642cbc8489fbb3ca4de41012ae0be7
Nora <norafigueroafwd@gmail.com>
2026-03-02 09:14:15 -0600

    Cambios Nora

--- frontend/src/routes/Routing.jsx
+++ frontend/src/routes/Routing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterEstudiantes from '../pages/RegisterEstudiantes';
+import RegisterEmpresas from '../pages/RegisterEmpresas';
 
 function Routing() {
   return (
@@ -9,7 +10,9 @@ function Routing() {
             {/* Rutas Públicas */}
             {/* <Route path="/" element={<Home/>}/> */}
             <Route path="/RegisterEstudiantes" element={<RegisterEstudiantes/>}/>
+            <Route path="/RegisterEmpresas" element={<RegisterEmpresas/>}/>
 
+            RegisterEmpresas
         </Routes>
     </Router> 
   )
