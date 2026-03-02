95f86c9e8c5f667c0198692d12ad420ef521707f
Eli-Jiron <elianguevara28@gmail.com>
2026-03-02 12:00:26 -0600

    urls de las apps notificaciones, postulaciones, vacantes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,6 +5,9 @@ api_urls = [
     path("auth/", include("apps.authentication.urls")),
     path("common/", include("apps.common.urls")),
     path("documents/", include("apps.Documents.urls")),
+    path("postulations/", include("apps.Postulations.urls")),
+    path("vacancies/", include("apps.Vacancies.urls")),
+    path("notifications/", include("apps.Notificattions.urls")),
 ]
 
 urlpatterns = [path("admin/", admin.site.urls), path("api/", include(api_urls))]
