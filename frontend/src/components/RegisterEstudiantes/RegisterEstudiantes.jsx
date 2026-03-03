5ce01e70840fe0e94430a92de772bbc4653ebcf0
Garrido-1910 <c.garridofwd19@gmail.com>
2026-03-03 10:50:55 -0600

    Ultima versión funcional

--- frontend/src/components/RegisterEstudiantes/RegisterEstudiantes.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import FormRegisterEstudiantes from '../components/forms/Register estudiantes/FormRegisterEstudiantes';
-
-function RegisterEstudiantes() {
-  return <FormRegisterEstudiantes />;
-}
-
-export default RegisterEstudiantes;
