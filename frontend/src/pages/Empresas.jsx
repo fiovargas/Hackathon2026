668eaf42a95a11ff6f86c2490d9ea87437948b05
Nora <norafigueroafwd@gmail.com>
2026-03-03 11:16:22 -0600

    pagina de empresas

---
+++ frontend/src/pages/Empresas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../components/Empresas/Dashboard/Dashboard';
+
+function Empresas() {
+    return (
+        <div>
+            <Dashboard />
+        </div>
+    );
+}
+
+export default Empresas;
